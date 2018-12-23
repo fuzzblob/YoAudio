e20b538eeda82e55a689209828e6f9c28deeca1b
Maris <maris@tammik.ca>
2018-12-23 19:33:43 +1300

    - updated YoaConfig.h default

---
+++ include/YoaConfig.h
@@ -0,0 +1,12 @@
+#ifndef _YOA_VERSION_H
+#define _YOA_VERSION_H
+
+// version number set through CMake prejoect build process
+#define YOA_VERSION_MAJOR 0
+#define YOA_VERSION_MINOR 0
+
+// toggle certain features of YoAudio
+#define SDL2_ENABLED true
+#define SDPLOG_ENABLED true
+
+#endif // _YOA_VERSION_H
