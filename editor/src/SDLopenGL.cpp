d4d617ad1fb80ffbc923d7a8ee5ba9ad46db77ec
maris <admin@fuzzblob.com>
2025-03-15 00:09:01 +0100

    SDLopenGL.cpp fixes
    - fix external headers in SDLopenGL.cpp
    - clean up commented out example implementation

--- editor/src/SDLopenGL.cpp
+++ editor/src/SDLopenGL.cpp
@@ -1,9 +1,9 @@
-#include "SDL.h"
-
 #include "SDLopenGL.h"
-#include "imgui.h"
-#include "imgui_impl_sdl2.h"
-#include "imgui_impl_opengl3.h"
+
+#include <SDL.h>
+#include <imgui.h>
+#include <imgui_impl_sdl2.h>
+#include <imgui_impl_opengl3.h>
 
 SDLopenGL::SDLopenGL() noexcept
 {
@@ -180,8 +180,7 @@ SDLopenGL::~SDLopenGL()
 //    SDL_DestroyWindow(mWindow);
 //    SDL_Quit();
 //}
-
-
+//
 // // Main code
 // int MainLoop(int, char**)
 // {
@@ -189,7 +188,7 @@ SDLopenGL::~SDLopenGL()
 //     bool show_demo_window = true;
 //     bool show_another_window = false;
 //     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
+//
 //     // Main loop
 //     bool done = false;
 //     while (!done)
@@ -213,39 +212,39 @@ SDLopenGL::~SDLopenGL()
 //             SDL_Delay(10);
 //             continue;
 //         }
-
+//
 //         // Start the Dear ImGui frame
 //         ImGui_ImplOpenGL3_NewFrame();
 //         ImGui_ImplSDL2_NewFrame();
 //         ImGui::NewFrame();
-
+//
 //         // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
 //         if (show_demo_window)
 //             ImGui::ShowDemoWindow(&show_demo_window);
-
+//
 //         // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
 //         {
 //             static float f = 0.0f;
 //             static int counter = 0;
-
+//
 //             ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
+//
 //             ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
 //             ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
 //             ImGui::Checkbox("Another Window", &show_another_window);
-
+//
 //             ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
 //             ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
+//
 //             if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
 //                 counter++;
 //             ImGui::SameLine();
 //             ImGui::Text("counter = %d", counter);
-
+//
 //             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
 //             ImGui::End();
 //         }
-
+//
 //         // 3. Show another simple window.
 //         if (show_another_window)
 //         {
@@ -255,7 +254,7 @@ SDLopenGL::~SDLopenGL()
 //                 show_another_window = false;
 //             ImGui::End();
 //         }
-
+//
 //         // Rendering
 //         ImGui::Render();
 //         glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
