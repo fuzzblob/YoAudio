92ff5fd3de881129d8bfe33d8b6c3159c3af8c86
maris <admin@fuzzblob.com>
2025-04-04 20:47:45 +0200

    panning fix
    - implement better pan law in StereoPanner

--- engine/src/StereoPanner.h
+++ engine/src/StereoPanner.h
@@ -11,8 +11,9 @@ namespace YoaEngine
 		LinearSmooothValue Pan;
 
 		// TODO(maris): check is this to do with pan law?
-		float volL = 0.77f;
-		float volR = 0.77f;
+		static constexpr float centerPan = 0.75f;
+		float volL = centerPan;
+		float volR = centerPan;
 
 		StereoPanner() noexcept {
 			Pan.Reset(0.0f);
@@ -25,8 +26,8 @@ namespace YoaEngine
 
 		void CalculateNext() {
 			const float t = Pan.GetNext();
-			volL = sqrt(0.5f * (1.0f - t));
-			volR = sqrt(0.5f * (1.0f + t));
+			volL = 1 - pow(0.5 * (1 + t), 2); // sqrt(0.5f * (1.0f - t));
+			volR = 1 - pow(0.5 * (1 - t), 2); // sqrt(0.5f * (1.0f + t));
 		}
 
 		float ReturnCurrentPan(){
