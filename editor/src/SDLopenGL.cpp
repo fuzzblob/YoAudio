14d1be90b0d7ef15b1a806dd02d4204d949532d9
maris <admin@fuzzblob.com>
2025-04-05 01:43:40 +0200

    enable printf in SDLopenGL

--- editor/src/SDLopenGL.cpp
+++ editor/src/SDLopenGL.cpp
@@ -1,13 +1,17 @@
 #include "SDLopenGL.h"
 
+//rendering
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <SDL_opengles2.h>
 #else
 #include <SDL_opengl.h>
 #endif
+// gui ibrary
 #include <imgui.h>
 #include <imgui_impl_sdl2.h>
 #include <imgui_impl_opengl3.h>
+// printf
+#include <stdio.h>
 
 namespace YoaEditor
 {
