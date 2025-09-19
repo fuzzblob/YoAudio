4babe2ebdcdd202946eb359a6a2a242bba447da8
fuzzblob <admin@fuzzblob.com>
2025-09-19 18:55:31 +0200

    put CPU architecture switching in FindSDL2.cmake

--- build-scripts/build_Windows_VisualStudio2022.bat
+++ build-scripts/build_Windows_VisualStudio2022.bat
@@ -15,6 +15,6 @@ cd build_MSVC
 set VS150COMNTOOLS = "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\"
 rem run CMake
 rem change the target project type if not using VisualStudio
-%CM% -DCPM_SOURCE_CACHE=../.cpm-cache/ -G "Visual Studio 17 2022" -A x64 -Wdev -S ../../
+%CM% -DCPM_SOURCE_CACHE=../.cpm-cache/ -G "Visual Studio 17 2022" -Wdev -S ../../
 
 cd ..
